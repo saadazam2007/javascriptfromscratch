0b434ac3455c527c7f2fda457dcea2a0def8b92c
unknown <saadazamkhan5900@gmail.com>
2024-03-12 15:48:32 +0500

    my first javascript program about comments and variable declaration types was created

---
+++ 00_basics/variables.js
@@ -0,0 +1,10 @@
+console.log("Saad")
+//variables
+const $name= "Saad"
+let user_name= "Js_practice00"
+var pass= "saadii2007"
+email= "john@google.com"
+// const cannot be changed it's value is constant
+// var is global it doesn't go well with blockscope
+// let is the most commonly used declaration method to assign a value to a variable 
+console.table([$name, user_name, pass,email])
