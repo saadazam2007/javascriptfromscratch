a54da7498994bc3f580b8059ae078583c398485c
unknown <saadazamkhan5900@gmail.com>
2024-03-12 16:15:34 +0500

    javascript program about code readability &  datatypes was created

---
+++ 00_basics/datatypesbasics.js
@@ -0,0 +1,25 @@
+"use strict" //treat all the code as the newer version
+//alert (3+3) //it is only applicable on browser not possible using node.js
+ 
+// priortize code readability
+
+let name = "Saad"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (empty value)
+// undefined => a value is not assigned
+// symbol => for uniqueness
+
+
+
+// object
+ //typeof is used to find out the datatype of any value
+console.log(typeof null) // null is an object
+console.log(typeof undefined)// undefined is undefined
+console.log(typeof age)
+console.log(typeof name)
