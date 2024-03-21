a71c484a66566eb1c78d3fc496245b168e45db9c
unknown <saadazamkhan5900@gmail.com>
2024-03-21 11:20:07 +0500

    comparison.js  was added

---
+++ 00_basics/comparison.js
@@ -0,0 +1,29 @@
+ //basic num comparison
+ console.log(2>1);
+ console.log(2<1);
+ console.log(2>=1); 
+ console.log(2<=1);
+ console.log(2==1);
+ console.log(2!=1);
+
+
+ //null and undefined comparison
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(null==0);
+console.log(null!=0);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+console.log(undefined==0);
+console.log(undefined!=0);
+//we should never prefer to use comparison with diff datatypes and the above two null and undefined
+
+
+
+// for strict check ""==="" is used
+ console.log("2" === 2);
