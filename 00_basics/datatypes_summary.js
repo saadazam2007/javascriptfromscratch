3547b968af1f15151bc5f0910118a2e4b8632161
unknown <saadazamkhan5900@gmail.com>
2024-03-21 15:36:40 +0500

    datatypes_summary.js was modified

--- 00_basics/datatypes_summary.js
+++ 00_basics/datatypes_summary.js
@@ -37,3 +37,25 @@ const myfunc = function name(params) {
 console.log(typeof (myfunc));
 
 //all non primitive when typeof is used result is object except func it is objectfunction.
+
+
+//Memory are  of two types one is stack and second is heap
+/* Stack is used in primitive datatypes and heap is used in non-primitive datatypes*/
+/* in primitive types you get the copied version of the original one when changes are made only affect the copied version
+like:
+let a = 1
+let b = a
+let b = 2
+the result of b will be 2 while a will not be changed and when we take b = a a copy of a
+will be given to b not the reference point*/ 
+/* in non-primitive the original reference is stored every change will take effect on the original value
+like:
+let user1 = {
+    name: "saad",
+    email: "saadazamkhan5900@gmail.com",
+}  
+let user2 = user1
+here copy won't be given instead the reference point will be provided like the real value 
+now if we change something
+user2.name = "Anas"
+it will also take affect on user1*/
